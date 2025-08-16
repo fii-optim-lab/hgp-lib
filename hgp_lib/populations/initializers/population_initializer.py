bc81558ed6ad30983467d0c9cd229693469cc4be
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2025-08-16 21:51:49 +0300

    Added some explanations for how will we handle externally declared population initialization

--- hgp_lib/populations/initializers/population_initializer.py
+++ hgp_lib/populations/initializers/population_initializer.py
@@ -14,3 +14,12 @@ class PopulationInitializer(ABC):
     @abstractmethod
     def generate(self):
         pass
+
+# TODO: Document how to pass the population initializer to the orchestrator.
+#  * The plan is like this:
+#    - either pass a string literal denoting one of the already implemented initializers
+#    - or pass a type, inheriting from PopulationInitializer and implementing generate
+#       * if passing a type, the user is also required to pass initializer_kwargs, which will be used to initialize the
+#       population; we will also try to do internally introspection to match missing kwarg-values for kwargs already
+#       used in our implemented population initializers; e.g. if the user uses `data` for __init__, but doesn't have a
+#       `data` key in the initializer_kwargs, we will provide the `data`
