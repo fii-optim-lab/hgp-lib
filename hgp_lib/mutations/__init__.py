17ab8df7310c913f395d3172c7c4a9031dd20ada
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2025-10-27 20:13:27 +0200

    Prepared __init__.py for mutations

---
+++ hgp_lib/mutations/__init__.py
@@ -0,0 +1,19 @@
+from .base_mutation import Mutation
+from .literal_mutations import DeleteMutation, NegateMutation, ReplaceLiteral, PromoteLiteral, \
+    create_standard_literal_mutations
+from .operator_mutations import RemoveIntermediateOperator, ReplaceOperator, AddLiteral, \
+    create_standard_operator_mutations
+from .mutation_executor import MutationExecutor
+from .utils import MutationError
+
+__all__ = [
+    "MutationExecutor", "Mutation", "MutationError",
+    # Factory methods
+    "create_standard_literal_mutations", "create_standard_operator_mutations",
+    # Literal and operator mutations
+    "DeleteMutation", "NegateMutation",
+    # Literal mutations
+    "ReplaceLiteral", "PromoteLiteral",
+    # Operator mutations
+    "RemoveIntermediateOperator", "ReplaceOperator", "AddLiteral"
+]
