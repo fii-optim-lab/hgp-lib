f1dd3ff17ec9a659ce31cb8d2d9c1a209808e7e1
ancestor-mithril <58839912+ancestor-mithril@users.noreply.github.com>
2025-12-05 15:37:12 +0200

    Added utils to all

--- hgp_lib/rules/__init__.py
+++ hgp_lib/rules/__init__.py
@@ -2,10 +2,11 @@ import os
 
 from .literals import Literal
 from .rules import Rule
+from . import utils
 
 if os.getenv("HPG_LOW_MEMORY", "0") == "1":
     from .low_memory_operators import Or, And
 else:
     from .operators import Or, And
 
-__all__ = ["Literal", "Rule", "Or", "And"]
+__all__ = ["Literal", "Rule", "Or", "And", "utils"]
