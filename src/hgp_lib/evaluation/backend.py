17539742481d9325e8f3ca6dbaf4d37d1b31db27
ancestor-mithril <58839912+ancestor-mithril@users.noreply.github.com>
2026-09-04 19:01:52 +0300

    Create an evaluation backend interface. Fixes #112

--- src/hgp_lib/evaluation/backend.py
+++ src/hgp_lib/evaluation/backend.py
@@ -1,9 +1,36 @@
 from abc import ABC, abstractmethod
+from collections.abc import Callable
 
 import numpy as np
+
+from ..rules import Rule
+from .evaluation_context import EvaluationContext
 
 
 class EvaluationBackend(ABC):
     @abstractmethod
-    def prepare(self, data: np.ndarray, labels: np.ndarray, score_fn):
+    def prepare_evaluation_context(
+        self,
+        data: np.ndarray,
+        labels: np.ndarray,
+        score_fn: Callable | None,
+        optimize_scorer: bool,
+    ) -> EvaluationContext:
+        pass
+
+
+    @abstractmethod
+    def evaluate_population(
+        self,
+        population: list[Rule],
+        evaluation: EvaluationContext,
+    ) -> np.ndarray:
+        pass
+
+    @abstractmethod
+    def evaluate_rule(
+        self,
+        rule: Rule,
+        data: np.ndarray,
+    ) -> np.ndarray:
         pass
